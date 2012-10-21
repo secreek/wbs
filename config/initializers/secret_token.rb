9b7c5ff6ea2dc8297117154f55e9ac62d1cc23c8
Cliff Woo <cliffwoo@gmail.com>
2012-10-21 16:13:24 +0800

    WBS Rails Architecture

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wbs::Application.config.secret_token = 'afac6341bdda81a92fd0306cd89246cd04f6f8256bca50ee8760b4b8268cefe5b17c69756b931499cb812827a01105cd6c80cfa8cfcda21412adbf908f2d7670'
